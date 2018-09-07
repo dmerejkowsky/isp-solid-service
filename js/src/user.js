012bd88fd7f2f2f5d918cd57c5944c32ec29f55d
Dimitri Merejkowsky <d.merej@gmail.com>
2018-09-07 20:47:32 +0200

    eslint ftw

--- js/src/user.js
+++ js/src/user.js
@@ -6,6 +6,11 @@ class User {
   name() {
     return this.data.name;
   }
+
+  address() {
+    const { address1, address2 } = this.data;
+    return `${address1}\n${address2}`;
+  }
 }
 
 module.exports = User;
--- js/test/user.spec.js
+++ js/test/user.spec.js
@@ -7,12 +7,22 @@ describe('User', () => {
   beforeEach(() => {
     this.data = {
       name: 'john',
+      address1: '452 Wilson Summit',
+      address2: 'East Dawnshier, AK 96919',
     }
   });
+
+  it('should have a name', () => {
+    const user = new User(this.data);
+    expect(user.name()).to.eq('john');
+  });
+
   context('Member', () => {
-    it('should have a name', () => {
+
+    it('should have an address', () => {
       const user = new User(this.data);
-      expect(user.name()).to.eq('john');
+      expect(user.address()).to.eq('452 Wilson Summit\nEast Dawnshier, AK 96919');
     });
   });
+
 });
