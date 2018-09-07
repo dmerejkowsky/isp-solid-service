1a138c9ea62097d05047c08d7c3851d4490137b2
Dimitri Merejkowsky <d.merej@gmail.com>
2018-09-07 21:06:29 +0200

    First implementation in Javascript

--- js/src/user.js
+++ js/src/user.js
@@ -4,12 +4,29 @@ class User {
   }
 
   name() {
-    return this.data.name;
+    return this.data.trial
+      ? this.data.tempLogin
+      : this.data.name;
   }
 
   address() {
     const { address1, address2 } = this.data;
     return `${address1}\n${address2}`;
+  }
+
+  ldapLogin() {
+    const { enterpriseName, name } = this.data;
+    return `${enterpriseName}/admin/${name}`;
+  }
+
+  daysLeft() {
+    const now = new Date().getTime();
+    const { createdAt } = this.data;
+    return this._fromMilis(now - createdAt);
+  }
+
+  _fromMilis(milis) {
+    return Math.floor(milis / (1000 * 60 * 60 * 24));
   }
 }
 
--- js/test/user.spec.js
+++ js/test/user.spec.js
@@ -7,8 +7,7 @@ describe('User', () => {
   beforeEach(() => {
     this.data = {
       name: 'john',
-      address1: '452 Wilson Summit',
-      address2: 'East Dawnshier, AK 96919',
+      enterpriseName: 'fooCorp',
     }
   });
 
@@ -18,11 +17,45 @@ describe('User', () => {
   });
 
   context('Member', () => {
-
     it('should have an address', () => {
+      this.data.member = true;
+      this.data.address1 = '452 Wilson Summit';
+      this.data.address2 = 'East Dawnshier, AK 96919';
       const user = new User(this.data);
       expect(user.address()).to.eq('452 Wilson Summit\nEast Dawnshier, AK 96919');
     });
   });
+
+  context('Admin', () => {
+    it('should have a LDAP login', () => {
+      const user = new User(this.data);
+      this.data.admin = true;
+      this.data.enterpriseName = 'fooCorp';
+      expect(user.ldapLogin()).to.eq('fooCorp/admin/john');
+    });
+  });
+
+  context('Trial User', () => {
+    const now = new Date().getTime();
+    const twoDaysAgo = new Date(now - 1000 * 60 * 60 * 24 * 2);
+
+    beforeEach(() => {
+      this.data.trial = true;
+      this.data.tempLogin = 'temp login';
+      this.data.createdAt = twoDaysAgo;
+    });
+
+    it('should have a temporary name', () => {
+      const user = new User(this.data);
+      expect(user.name()).to.eq('temp login');
+    });
+
+    it('should compute the number of days left', () => {
+      const user = new User(this.data);
+      expect(user.name()).to.eq('temp login');
+      expect(user.daysLeft()).to.eq(2);
+    });
+  });
+
 
 });
