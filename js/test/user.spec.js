eb250497a3c1f3e4c242f365a8ffdf68fb7eab22
Dimitri Merejkowsky <d.merej@gmail.com>
2018-09-21 20:34:04 +0200

    Replacing mocha by tape

--- js/test/user.spec.js
+++ js/test/user.spec.js
@@ -1,66 +1,65 @@
-const chai = require('chai');
-const { expect } = chai;
+// Allow using 't' as variable name for the tests
+/* eslint-disable id-length */
+
+const { test } = require('tape');
 
 const User = require('../src/user');
 const Member = require('../src/member');
 const Admin = require('../src/admin');
 const TrialUser = require('../src/trialUser');
 
-describe('users', () => {
-  beforeEach(() => {
-    this.data = {
-      name: 'john',
-    }
-  });
+test('Users should have a name', (t) => {
+  const data = {
+        name: 'john',
+  }
 
-  it('should have a name', () => {
-    const user = new User(this.data);
-    expect(user.name()).to.eq('john');
-  });
-
-  context('Member', () => {
-    beforeEach(() => {
-      const user = new User(this.data);
-      this.member = new Member(user);
-    });
-    it('should have an address', () => {
-      this.data.address1 = '452 Wilson Summit';
-      this.data.address2 = 'East Dawnshier, AK 96919';
-      expect(this.member.address()).to.eq('452 Wilson Summit\nEast Dawnshier, AK 96919');
-    });
-  });
-
-  context('Admin', () => {
-    beforeEach(() => {
-      const user = new User(this.data);
-      this.admin = new Admin(user);
-    });
-    it('should have a LDAP login', () => {
-      this.data.enterpriseName = 'fooCorp';
-      expect(this.admin.ldapLogin()).to.eq('fooCorp/admin/john');
-    });
-  });
-
-  context('Trial User', () => {
-
-    beforeEach(() => {
-      const now = new Date().getTime();
-      const twoDaysAgo = new Date(now - 1000 * 60 * 60 * 24 * 2);
-      this.data.tempLogin = 'temp login';
-      this.data.createdAt = twoDaysAgo;
-      this.trialUser = new TrialUser(this.data);
-    });
-
-    it('should have a temporary name', () => {
-      const user = new TrialUser(this.data);
-      expect(user.name()).to.eq('temp login');
-    });
-
-    it('should compute the number of days left', () => {
-      const user = new TrialUser(this.data);
-      expect(user.daysLeft()).to.eq(2);
-    });
-  });
+  const user = new User(data);
+  t.equal(user.name(), 'john');
+  t.end();
+});
 
 
+test('Members should have an address', (t) => {
+  const data = {
+    name: 'john',
+    address1: '452 Wilson Summit',
+    address2: 'East Dawnshier, AK 96919',
+  };
+  const user = new User(data);
+  const member = new Member(user);
+  t.equal(member.address(), '452 Wilson Summit\nEast Dawnshier, AK 96919');
+  t.end();
+});
+
+test('Admins should have a LDAP login', (t) => {
+  const data = {
+    name: 'john',
+    enterpriseName: 'fooCorp',
+  };
+  const user = new User(data);
+  const admin = new Admin(user);
+  t.equal(admin.ldapLogin(), 'fooCorp/admin/john');
+  t.end();
+});
+
+test('Trial users have a temporary name', (t) => {
+  const data = {
+    name: 'john',
+    tempLogin: 'temp login',
+  };
+  const user = new TrialUser(data);
+  t.equal(user.name(), 'temp login');
+  t.end();
+});
+
+test('Compute numbers of days left for trial users', (t) => {
+  const now = new Date().getTime();
+  const twoDaysAgo = new Date(now - 1000 * 60 * 60 * 24 * 2);
+  const data = {
+    tempLogin: 'temp login',
+    createdAt: twoDaysAgo,
+  };
+  const trialUser = new TrialUser(data);
+  t.equal(trialUser.daysLeft(), 2);
+  t.end();
 });
